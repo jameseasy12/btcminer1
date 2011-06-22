30a1f279afaf685341974cd6094b2d16d44b8c58
Ryan Fairchild <ryan.fairchild@gmail.com>
2011-06-22 13:35:31 -0400

    Testing kernel install

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(name="gpu_miner",
       version="2011.beta4",
       packages=['miner'],
-			package_data = { 'miner': ['kernel.cl'] },
+			package_data = { '': ['kernel.cl'] },
 			include_package_data = True,
       entry_points=dict(console_scripts=['gpu_miner=miner:main', 'gpu_miner-%s=miner:main' % sys.version[:3]]),
       zip_safe=False)
