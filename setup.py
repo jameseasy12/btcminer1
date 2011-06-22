cabae55638e5c7505c99e8c5c4760e4bf31d15d0
Ryan Fairchild <ryan.fairchild@gmail.com>
2011-06-22 14:35:31 -0400

    Removing numpy dep, as it must be installed in a separate step

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(name="gpu_miner",
       packages=['miner'],
 			package_data = { 'miner': ['kernel.cl'] },
       entry_points=dict(console_scripts=['gpu_miner=miner:main', 'gpu_miner-%s=miner:main' % sys.version[:3]]),
-			install_requires = ['pyopencl==2011.1beta3', 'pycrypto>=2.3', 'jsonrpc>=0.99a01'],
-			setup_requires= ['numpy>=1.6.0'],
+			install_requires = ['pyopencl==2011.1beta3', 'jsonrpc>=0.99a01'],
       zip_safe=False)
