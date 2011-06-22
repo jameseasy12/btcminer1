75171850f91b3072f9fe58820797365674388745
Ryan Fairchild <ryan.fairchild@gmail.com>
2011-06-22 13:23:24 -0400

    Adding kernel into setup

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup, find_packages
 setup(name="gpu_miner",
       version="2011.beta4",
       packages=['miner'],
-			package_data = {
-				'miner': ['*.cl']
-			},
+			package_data = { 'miner': ['kernel.cl'] },
+			include_package_data = True
       entry_points=dict(console_scripts=['gpu_miner=miner:main', 'gpu_miner-%s=miner:main' % sys.version[:3]]),
-			install_requires = ['numpy>=1.6.0','pyopencl==2011.1beta3', 'pycrypto>=2.3', 'jsonrpc>=0.99a01'],
+			install_requires = ['pyopencl==2011.1beta3', 'pycrypto>=2.3', 'jsonrpc>=0.99a01'],
+			setup_requires= ['numpy>=1.6.0'],
       zip_safe=False)
