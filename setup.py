eeec87a5e64484ae532f94a3844e174ab6c9ecfd
Ryan Fairchild <ryan.fairchild@gmail.com>
2011-06-22 13:31:30 -0400

    Testing kernel install

--- setup.py
+++ setup.py
@@ -7,6 +7,4 @@ setup(name="gpu_miner",
 			package_data = { 'miner': ['kernel.cl'] },
 			include_package_data = True,
       entry_points=dict(console_scripts=['gpu_miner=miner:main', 'gpu_miner-%s=miner:main' % sys.version[:3]]),
-			install_requires = ['pyopencl==2011.1beta3', 'pycrypto>=2.3', 'jsonrpc>=0.99a01'],
-			setup_requires= ['numpy>=1.6.0'],
       zip_safe=False)
